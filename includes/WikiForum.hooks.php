9bcc5620a9bd44aa6fb354bf3c6a88907eebf11b
SamanthaNguyen <samanthanguyen1116@gmail.com>
2017-12-27 16:01:25 -0600

    Use newer DatabaseUpdater functions
    Change-Id: I4fc1eb956f78917ca6fd832c5c21a8fd5d3ec57e

--- includes/WikiForum.hooks.php
+++ includes/WikiForum.hooks.php
@@ -109,57 +109,57 @@ class WikiForumHooks {
 		$dir = dirname( __FILE__ ) . '/../sql';
 		$file = "$dir/wikiforum.sql";
 
-		$updater->addExtensionUpdate( array( 'addTable', 'wikiforum_category', $file, true ) );
-		$updater->addExtensionUpdate( array( 'addTable', 'wikiforum_forums', $file, true ) );
-		$updater->addExtensionUpdate( array( 'addTable', 'wikiforum_threads', $file, true ) );
-		$updater->addExtensionUpdate( array( 'addTable', 'wikiforum_replies', $file, true ) );
+		$updater->addExtensionTable( 'wikiforum_category', $file );
+		$updater->addExtensionTable( 'wikiforum_forums', $file );
+		$updater->addExtensionTable( 'wikiforum_threads', $file );
+		$updater->addExtensionTable( 'wikiforum_replies', $file );
 
 		// upgrade from pre 1.3.0-SW
 		if ( !$updater->getDB()->fieldExists( 'wikiforum_category', 'wfc_added_user_ip' ) ) {
 			$file = $dir . '/1.3.0-SW-new-fields.sql';
 			// wikiforum_category
-			$updater->addExtensionUpdate( array( 'addField', 'wikiforum_category', 'wfc_added_user_ip', $file, true ) );
-			$updater->addExtensionUpdate( array( 'addField', 'wikiforum_category', 'wfc_edited_user_ip', $file, true ) );
+			$updater->addExtensionField( 'wikiforum_category', 'wfc_added_user_ip', $file );
+			$updater->addExtensionField( 'wikiforum_category', 'wfc_edited_user_ip', $file );
 			// wikiforum_forums
-			$updater->addExtensionUpdate( array( 'addField', 'wikiforum_forums', 'wff_last_post_user_ip', $file, true ) );
-			$updater->addExtensionUpdate( array( 'addField', 'wikiforum_forums', 'wff_added_user_ip', $file, true ) );
-			$updater->addExtensionUpdate( array( 'addField', 'wikiforum_forums', 'wff_edited_user_ip', $file, true ) );
+			$updater->addExtensionField( 'wikiforum_forums', 'wff_last_post_user_ip', $file );
+			$updater->addExtensionField( 'wikiforum_forums', 'wff_added_user_ip', $file );
+			$updater->addExtensionField( 'wikiforum_forums', 'wff_edited_user_ip', $file );
 			// wikiforum_threads
-			$updater->addExtensionUpdate( array( 'addField', 'wikiforum_threads', 'wft_user_ip', $file, true ) );
-			$updater->addExtensionUpdate( array( 'addField', 'wikiforum_threads', 'wft_edit_user_ip', $file, true ) );
-			$updater->addExtensionUpdate( array( 'addField', 'wikiforum_threads', 'wft_closed_user_ip', $file, true ) );
-			$updater->addExtensionUpdate( array( 'addField', 'wikiforum_threads', 'wft_last_post_user_ip', $file, true ) );
+			$updater->addExtensionField( 'wikiforum_threads', 'wft_user_ip', $file );
+			$updater->addExtensionField( 'wikiforum_threads', 'wft_edit_user_ip', $file );
+			$updater->addExtensionField( 'wikiforum_threads', 'wft_closed_user_ip', $file );
+			$updater->addExtensionField( 'wikiforum_threads', 'wft_last_post_user_ip', $file );
 			// wikiforum_replies
-			$updater->addExtensionUpdate( array( 'addField', 'wikiforum_replies', 'wfr_user_ip', $file, true ) );
-			$updater->addExtensionUpdate( array( 'addField', 'wikiforum_replies', 'wfr_edit_user_ip', $file, true ) );
+			$updater->addExtensionField( 'wikiforum_replies', 'wfr_user_ip', $file );
+			$updater->addExtensionField( 'wikiforum_replies', 'wfr_edit_user_ip', $file );
 		}
 
 		// Upgrade from post 1.3.0-SW and pre 2.0.0
 		else if ( $updater->getDB()->fieldExists( 'wikiforum_category', 'wfc_added_user_user_text' ) ) {
 			$file = $dir . '/2.0.0-drop-fields.sql';
 			// wikiforum_category
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_category', 'wfc_added_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_category', 'wfc_edited_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_category', 'wfc_deleted_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_category', 'wfc_deleted_user_ip', $file, true ) );
+			$updater->dropExtensionField( 'wikiforum_category', 'wfc_added_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_category', 'wfc_edited_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_category', 'wfc_deleted_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_category', 'wfc_deleted_user_ip', $file );
 			// wikiforum_forums
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_forums', 'wff_last_post_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_forums', 'wff_added_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_forums', 'wff_edited_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_forums', 'wff_deleted_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_forums', 'wff_deleted_user_ip', $file, true ) );
+			$updater->dropExtensionField( 'wikiforum_forums', 'wff_last_post_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_forums', 'wff_added_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_forums', 'wff_edited_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_forums', 'wff_deleted_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_forums', 'wff_deleted_user_ip', $file );
 			// wikiforum_threads
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_threads', 'wft_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_threads', 'wft_deleted_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_threads', 'wft_deleted_user_ip', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_threads', 'wft_edit_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_threads', 'wft_closed_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_threads', 'wft_last_post_user_text', $file, true ) );
+			$updater->dropExtensionField( 'wikiforum_threads', 'wft_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_threads', 'wft_deleted_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_threads', 'wft_deleted_user_ip', $file );
+			$updater->dropExtensionField( 'wikiforum_threads', 'wft_edit_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_threads', 'wft_closed_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_threads', 'wft_last_post_user_text', $file );
 			// wikiforum_replies
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_replies', 'wfr_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_replies', 'wfr_deleted_user_text', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_replies', 'wfr_deleted_user_ip', $file, true ) );
-			$updater->addExtensionUpdate( array( 'dropField', 'wikiforum_replies', 'wfr_edit_user_text', $file, true ) );
+			$updater->dropExtensionField( 'wikiforum_replies', 'wfr_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_replies', 'wfr_deleted_user_text', $file );
+			$updater->dropExtensionField( 'wikiforum_replies', 'wfr_deleted_user_ip', $file );
+			$updater->dropExtensionField( 'wikiforum_replies', 'wfr_edit_user_text', $file );
 		}
 
 		return true;
